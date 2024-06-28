44ea37c4fef8e517933ebdd13991153545e4060f
Mauricio_Mores <mauri.f.mores@gmail.com>
2024-06-27 21:28:53 -0300

    first commit

---
+++ src/main/java/com/alura/oracleOne/CatalogoDeLibrosApplication.java
@@ -0,0 +1,13 @@
+package com.alura.oracleOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoDeLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoDeLibrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/oracleOne/CatalogoDeLibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.oracleOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoDeLibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
